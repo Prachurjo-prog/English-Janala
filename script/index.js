407a859f367cc0ccb55edb36b57cc518ef7a0524
noob-prachurjo <prachurjobarmon2008@gmail.com>
2026-03-05 01:26:53 +0600

    here we worked at wordsCardSection & fetching data

--- script/index.js
+++ script/index.js
@@ -4,6 +4,39 @@ const loadLeason = () =>{
     .then(json => displayLesson(json.data));
 }
 
+const loadLesson = (id) =>{
+    console.log(id)
+    const URL = `https://openapi.programming-hero.com/api/level/${id}`;
+    
+    fetch(URL)
+    .then(res =>res.json())
+    .then(json => displayLessonWords(json.data))
+}
+
+const displayLessonWords = (words)=>{
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    for(let word of words){
+        const wordDiv = document.createElement("div");
+
+        wordDiv.innerHTML = `
+        <div class="bg-white p-5 rounded-lg text-center justify-center items-center">
+                <h1 class="text-2xl font-bold mb-2">${word.word}</h1>
+                 <p class=" font-medium mb-3">${"meaning"} / ${"pronunciation"}</p>
+                <p class="text-2xl font-medium bangla">"${word.meaning} / ${word.pronunciation}"</p>
+                
+                <div class="flex justify-between">
+                    <button class="btn bg-sky-50 hover:bg-sky-100 hover:cursor-pointer p-2 rounded-sm"><i class="fa-regular fa-circle-question "></i></button>
+                    <a class="btn bg-sky-50 hover:bg-sky-100 hover:cursor-pointer p-2 rounded-sm"><i class="fa-solid fa-volume-high"></i></a>
+                </div>
+            </div>
+        `
+        wordContainer.append(wordDiv);
+    }
+
+}
+
 const displayLesson = (lessons)=>{
           // 1 get the container and empty  
     const lessoncontainer = document.getElementById("lesson-container")
@@ -14,13 +47,15 @@ const displayLesson = (lessons)=>{
 
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-         <button class="btn btn-outline btn-primary mt-3">
-         <i class="fa-solid fa-book-open"></i> Learn - ${lesson.level_no}
+         <button onclick = "loadLesson(${lesson.level_no})" class="btn btn-outline btn-primary mt-3">
+         <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
          </button>
         `
         lessoncontainer.append(btnDiv);
     }
-    
+};
 
-}
+
+
+
 loadLeason();
